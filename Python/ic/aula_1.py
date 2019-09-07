3157c2608ab6fe1aeaa77c9a59dba46e3ef81960
Isaque Fernando <isaquefernando10@gmail.com>
2019-09-07 15:05:07 -0300

    Atualização do código para a aula 1. Preciso verificar o erro que continua acontecendo.

--- Python/ic/aula_1.py
+++ Python/ic/aula_1.py
@@ -7,9 +7,10 @@ import csv # Biblioteca utilizada para operar coma arquivos csv.
 def media(total, n):
     return total/n
 
-total = 0 # Declaração da variável para contar o total
-n = 0 # Declaração de variável para contar a quantidade de elementos
-flag = True # Variavel para eliminar a primeira linha
+total = 0 # Declaração da variável para contar o total.
+n = 0 # Declaração de variável para contar a quantidade de elementos.
+flag = True # Variavel para eliminar a primeira linha.
+lista = [] # Lista em python.
 
 # Abertura de arquivo utilizando a cláusula with. Ela cuida do erro na abertura, garante o fechamento e cuida dos demais erros.
 with open('../datasets/vgsales.csv', 'rt') as arq_teste:
@@ -23,9 +24,23 @@ with open('../datasets/vgsales.csv', 'rt') as arq_teste:
     print("Numero de elementos: {}".format(n)) # Exibindo o número de elementos.
     print(media(total, n))
 
+    # Com o arquivo aberto iremos novamente iterar sobre ele com o intuito de obter a mediana.
+    flag = True # Iremos novamente ignorar a primeira linha.
+    for linha in arq:
+        if flag:
+            flag = False
+        else:
+            lista.append(linha[6]) # Adicionamos os valores na última posição da lista.
+            # Lembrando que aqui estamos pegando os valores de vendas por ano.
+    
+    # Ao final teremos uma lista com todos os valores.
+    lista.sort() # Ordenamos a lista de forma crescente.
+    if (len(lista) % 2) == 0: # Caso a quantidade de elementos na lista seja par.
+        i = int(len(lista) / 2) # Vamos pegar os valores intermediários para calcular a mediana.
+        mediana = (lista[i-1] + lista[i]) / 2
+        print("A mediana eh: {}".format(mediana))
+    else: # Caso seja impar.
+        i = int(len(lista) / 2) # A função int transforma o número retornado em um inteiro. Utiliza a função piso.
+        mediana = lista[i]
+        print("A mediana eh: {}".format(mediana))
 
-# Função para cálcular a média.
-def media(total, n):
-    return total/n
-        
-        
