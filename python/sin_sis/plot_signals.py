f0f4867c6bbc716a402ae34edf80b66a5d5e94bf
Isaque Fernando <isaquefernando10@gmail.com>
2020-09-09 11:39:17 -0300

    Adicionando repositório para os scripts de sin_sis

---
+++ python/sin_sis/plot_signals.py
@@ -0,0 +1,9 @@
+# Script para plotar alguns sinais para a disciplina de sinais e sistemas
+import matplotlib.pyplot as plt
+
+x = [-3, -2, -1, 0, 1, 2, 3]
+# y = [element - 1 for element in x]  # x - 1
+y = x
+
+plt.plot(x, y)
+plt.show()
